b38982b31f98b51ca37da800497cac08afec7dd3
JooYoung <sana2dang@naver.com>
2017-04-20 22:34:34 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,6 +5,6 @@ sudo cp -r -v ./ES_fullRomList/* /home/pi/RetroPie/roms
 #sudo chgrp -R -v pi /home/pi/RetroPie/roms
 #sudo chown -R -v pi /home/pi/RetroPie/roms
 
-find /home/pi/RetroPie/roms -user root | while read line; do sudo chmod 755 $line; done 
-find /home/pi/RetroPie/roms -user root | while read line; do sudo chgrp -v pi $line; done 
-find /home/pi/RetroPie/roms -user root | while read line; do sudo chown -v pi $line; done
+find /home/pi/RetroPie/roms -user root | while read line; do sudo chmod 755 "$line"; done 
+find /home/pi/RetroPie/roms -user root | while read line; do sudo chgrp -v pi "$line"; done 
+find /home/pi/RetroPie/roms -user root | while read line; do sudo chown -v pi "$line"; done
